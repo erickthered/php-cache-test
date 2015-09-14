4743ebb64d301ab788fb2e30d599756958357a4b
Erick Rojas <erickrojaz@gmail.com>
2015-09-14 11:34:20 -0500

    first commit

---
+++ cache.php
@@ -0,0 +1,30 @@
+<?php
+
+class Cache {
+	protected static $_target = '';
+
+	public static function start()
+	{
+		self::$_target = $_SERVER['REQUEST_URI'];
+		if ($target == '/') {
+			$target = 'index.php';
+		}
+		self::$_target = './cached/'.self::$_target.'.html';
+
+		if (file_exists(self::$_target)) {
+			error_log('hits cache in PHP');
+			echo file_get_contents(self::$_target);
+			exit();
+		}
+
+		ob_start();
+	}
+
+	public static function flush()
+	{
+		$content = ob_get_contents();
+
+		mkdir(dirname(self::$_target), 0777, true);
+		file_put_contents(self::$_target, $content);
+	}
+}
---
+++ index2.php
@@ -0,0 +1,14 @@
+<?php
+include ('cache.php');
+Cache::start();
+
+echo '<!DOCTYPE html><html lang="es"><head><title>Cache Test</title></head><body>';
+echo '<h1>System Time: '.time().'</h1>';
+echo "<ul>";
+for ($i=0; $i<2000; $i++) {
+	echo '<li>Item number: '.$i.'</li>';
+}
+echo "</ul>";
+echo "</body><html>";
+
+Cache::flush();
